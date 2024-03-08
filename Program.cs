85db9fff29716bb07655b716412b59368d9eaf33
Roshan Chathuranga <147641397+Roshan-Coding@users.noreply.github.com>
2024-03-08 09:28:49 +0530

    print a statemant

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hello{
+    class Hello{
+        static void Main(String[] args){
+            Console.WriteLine("Hello World welcome to the C# program");
+            Console.WriteLine("I am Roshan Chathuranga");
+            Console.WriteLine(2*35);
+        }
+    }
+}
+
+            
+
