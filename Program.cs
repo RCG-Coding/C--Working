15382b373bf43facdeaccc1c9bfaa5bf27369c51
Roshan Chathuranga <147641397+Roshan-Coding@users.noreply.github.com>
2024-03-08 21:22:47 +0530

    create a simple calculator

--- Program.cs
+++ Program.cs
@@ -1,11 +1,40 @@
 ﻿using System;
 
-namespace hello{
-    class Hello{
+namespace SimpleCalculator{
+    class SC{
         static void Main(String[] args){
-            Console.WriteLine("Hello World welcome to the C# program");
-            Console.WriteLine("I am Roshan Chathuranga");
-            Console.WriteLine(2*35);
+            
+            Console.Write("Enter your first number : ");
+            string num1 = Console.ReadLine();
+            Console.Write("Enter your second number : ");
+            string num2 = Console.ReadLine();
+
+            if(!string.IsNullOrEmpty(num1) && !string.IsNullOrEmpty(num2)){
+                if(int.TryParse(num1, out int n1)&& int.TryParse(num2, out int n2)){
+                    int sum = n1 + n2;
+                    Console.WriteLine("Sum = "+ sum);
+
+                    int sub1 = n1 - n2;
+                    Console.WriteLine($"{n1} - {n2} = "+ sub1);
+
+                    int sub2 = n2 - n1;
+                    Console.WriteLine($"{n2} - {n1} = "+ sub2);
+
+                    int mul = n1 * n2;
+                    Console.WriteLine("Multiplication = "+ mul);
+
+                    double div1 = (double) n1 / n2;
+                    Console.WriteLine($"{n1} divide by {n2} = {div1:f3}");
+
+                    double div2 = (double) n2 / n1;
+                    Console.WriteLine($"{n2} divide by {n1} = {div2:f3}");
+
+                }else{
+                    Console.WriteLine("This is not integer number please enter the integer number for calculation");
+                }
+            }else{
+                Console.WriteLine("The Input number can not be null or empty, please enter the valied number");
+            }
         }
     }
 }
